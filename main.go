1ba1d78d813f1b4f280d61361e8516919570d320
Justin Clift <justin@postgresql.org>
2016-11-25 00:20:35 +0000

    Added stub (but working) user page to list their public databases

--- main.go
+++ main.go
@@ -137,7 +137,7 @@ func main() {
 }
 
 func mainHandler(w http.ResponseWriter, req *http.Request) {
-	pageName := "mainHandler()"
+	//pageName := "mainHandler()"
 
 	// Split the request URL into path components
 	pathStrings := strings.Split(req.URL.Path, "/")
@@ -150,240 +150,29 @@ func mainHandler(w http.ResponseWriter, req *http.Request) {
 		// Check if the request was for the root directory
 		if pathStrings[1] == "" {
 			// Yep, root directory request
-			// TODO: Create a template for the root directory
-			fmt.Fprintf(w, "Root directory")
+			renderRootPage(w)
 			return
 		}
 
-		// The request was for a user directory, so return that list
-		// TODO: Create a template for the user page
-		fmt.Fprintf(w, "Page for user: %s", userName)
+		// The request was for a user page
+		renderUserPage(w, userName)
 		return
 	}
 
 	userName := pathStrings[1]
 	databaseName := pathStrings[2]
 
-	// This catches the case where a "/" is on the end of the URL
+	// This catches the case where a "/" is on the end of a user page URL
 	// TODO: Refactor this and the above identical code.  Doing it this way is non-optimal
 	if pathStrings[2] == "" {
-		// The request was for a user directory, so return that list
-		// TODO: Create a template for the user page
-		fmt.Fprintf(w, "Page for user: %s", userName)
+		// The request was for a user page
+		renderUserPage(w, userName)
 		return
 	}
 
-	// Retrieve the MinioID, and the user visible info for the requested database
-	rows, err := db.Query(
-		"SELECT minioid, date_created, last_modified, size, version, public, watchers, stars, forks, "+
-			"discussions, pull_requests, updates, branches, releases, contributors, description, readme "+
-			"FROM public.sqlite_databases "+
-			"WHERE username = $1 "+
-			"AND dbname = $2 "+
-			"ORDER BY version DESC "+
-			"LIMIT 1",
-		userName, databaseName)
-	if err != nil {
-		log.Printf("%s: Database query failed: \n%v", pageName, err)
-		http.Error(w, "Database query failed", http.StatusInternalServerError)
-		return
-	}
-	defer rows.Close()
-	var dataRows dbInfo
-	for rows.Next() {
-		var Desc pgx.NullString
-		var Readme pgx.NullString
-		err = rows.Scan(&dataRows.MinioID, &dataRows.DateCreated, &dataRows.LastModified, &dataRows.Size,
-			&dataRows.Version, &dataRows.Public, &dataRows.Watchers, &dataRows.Stars, &dataRows.Forks,
-			&dataRows.Discussions, &dataRows.PRs, &dataRows.Updates, &dataRows.Branches, &dataRows.Releases,
-			&dataRows.Contributors, &Desc, &Readme)
-		if err != nil {
-			log.Printf("%s: Error retrieving MinioID from database: %v\n", pageName, err)
-			http.Error(w, "Error retrieving MinioID from database", http.StatusInternalServerError)
-			return
-		}
-		if !Desc.Valid {
-			dataRows.Description = "No description"
-		}
-		if !Readme.Valid {
-			dataRows.Readme = "No readme"
-		}
-	}
-	if dataRows.MinioID == "" {
-		log.Printf("%s: Requested database not found: %v for user: %v \n", pageName, databaseName, userName)
-		http.Error(w, "The requested database doesn't exist", http.StatusNotFound)
-		return
-	}
-
-	// Get a handle from Minio for the database object
-	userDB, err := minioClient.GetObject(userName, dataRows.MinioID)
-	if err != nil {
-		log.Printf("%s: Error retrieving DB from Minio: %v\n", pageName, err)
-		http.Error(w, "Error retrieving DB from Minio", http.StatusInternalServerError)
-		return
-	}
-
-	// Close the object handle when this function finishes
-	defer func() {
-		err := userDB.Close()
-		if err != nil {
-			log.Printf("%s: Error closing object handle: %v\n", pageName, err)
-		}
-	}()
-
-	// Save the database locally to a temporary file
-	tempfileHandle, err := ioutil.TempFile("", "databaseViewHandler-")
-	if err != nil {
-		log.Printf("%s: Error creating tempfile: %v\n", pageName, err)
-		http.Error(w, "Internal server error", http.StatusInternalServerError)
-		return
-	}
-	tempfile := tempfileHandle.Name()
-	bytesWritten, err := io.Copy(tempfileHandle, userDB)
-	if err != nil {
-		log.Printf("%s: Error writing database to temporary file: %v\n", pageName, err)
-		http.Error(w, "Internal server error", http.StatusInternalServerError)
-		return
-	}
-	if bytesWritten == 0 {
-		log.Printf("%s: 0 bytes written to the temporary file: %v\n", pageName, databaseName)
-		http.Error(w, "Internal server error", http.StatusInternalServerError)
-		return
-	}
-	tempfileHandle.Close()
-	defer os.Remove(tempfile) // Delete the temporary file when this function finishes
-
-	// Open database
-	db, err := sqlite.Open(tempfile, sqlite.OpenReadOnly)
-	if err != nil {
-		log.Printf("Couldn't open database: %s", err)
-		return
-	}
-	defer db.Close()
-
-	// Retrieve the list of tables in the database
-	tables, err := db.Tables("")
-	if err != nil {
-		log.Printf("Error retrieving table names: %s", err)
-		return
-	}
-	if len(tables) == 0 {
-		// No table names were returned, so abort
-		log.Printf("The database '%s' doesn't seem to have any tables. Aborting.", databaseName)
-		return
-	}
-	dataRows.Tables = tables
-
-	// Select the first table
-	selectedTable := dataRows.Tables[0]
-
-	// Retrieve (up to) x rows from the selected database
-	// Ugh, have to use string smashing for this, even though the SQL spec doesn't seem to say table names
-	// shouldn't be parameterised.  Limitation from SQLite's implementation? :(
-	stmt, err := db.Prepare("SELECT * FROM " + selectedTable + " LIMIT 8")
-	if err != nil {
-		log.Printf("Error when preparing statement for database: %s\v", err)
-		return
-	}
-
-	// Retrieve the field names
-	dataRows.TableHeaders = stmt.ColumnNames()
-
-	// Process each row
-	fieldCount := -1
-	err = stmt.Select(func(s *sqlite.Stmt) error {
-
-		// Get the number of fields in the result
-		if fieldCount == -1 {
-			fieldCount = stmt.DataCount()
-		}
-
-		// Retrieve the data for each row
-		var row []dataValue
-		for i := 0; i < fieldCount; i++ {
-			// Retrieve the data type for the field
-			fieldType := stmt.ColumnType(i)
-
-			switch fieldType {
-			case sqlite.Integer:
-				val, isNull, err := s.ScanInt(i)
-				if err != nil {
-					log.Printf("Something went wrong with ScanInt(): %v\n", err)
-					break
-				}
-				if !isNull {
-					stringVal := fmt.Sprintf("%d", val)
-					row = append(row, dataValue{Name: dataRows.TableHeaders[i], Type: Integer,
-						Value: stringVal})
-				} else {
-					row = append(row, dataValue{Name: dataRows.TableHeaders[i], Type: Null,
-						Value: "NULL"})
-				}
-			case sqlite.Float:
-				val, isNull, err := s.ScanDouble(i)
-				if err != nil {
-					log.Printf("Something went wrong with ScanDouble(): %v\n", err)
-					break
-				}
-				if !isNull {
-					stringVal := strconv.FormatFloat(val, 'f', 4, 64)
-					row = append(row, dataValue{Name: dataRows.TableHeaders[i], Type: Float,
-						Value: stringVal})
-				} else {
-					row = append(row, dataValue{Name: dataRows.TableHeaders[i], Type: Null,
-						Value: "NULL"})
-				}
-			case sqlite.Text:
-				val, isNull := s.ScanText(i)
-				if !isNull {
-					row = append(row, dataValue{Name: dataRows.TableHeaders[i], Type: Text,
-						Value: val})
-				} else {
-					row = append(row, dataValue{Name: dataRows.TableHeaders[i], Type: Null,
-						Value: "NULL"})
-				}
-			case sqlite.Blob:
-				val, isNull := s.ScanBlob(i)
-				if !isNull {
-					row = append(row, dataValue{Name: dataRows.TableHeaders[i], Type: Binary,
-						Value: val})
-				} else {
-					row = append(row, dataValue{Name: dataRows.TableHeaders[i], Type: Null,
-						Value: "NULL"})
-				}
-			case sqlite.Null:
-				row = append(row, dataValue{Name: dataRows.TableHeaders[i], Type: Null,
-					Value: "NULL"})
-			}
-		}
-		dataRows.Records = append(dataRows.Records, row)
-
-		return nil
-	})
-	if err != nil {
-		log.Printf("Error when retrieving select data from database: %s\v", err)
-		return
-	}
-	defer stmt.Finalize()
-
-	dataRows.Username = userName
-	dataRows.Database = databaseName
-	dataRows.Tablename = selectedTable
-	dataRows.Protocol = listenProtocol
-	dataRows.Server = listenAddr + ":9080"
-
-	// Parse the template, but use "[[" and "]]" as delimiters.  This is because both Go and AngularJS use
-	// "{{" "}}" by default, so one needs to be changed ;)
-	t := template.New("database.html")
-	t.Delims("[[", "]]")
-	t, err = t.ParseFiles("templates/database.html")
-	if err != nil {
-		log.Printf("Error: %s", err)
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	t.Execute(w, dataRows)
+	// A specific database was requested
+	// TODO: Add support for folders and sub-folders in request paths
+	renderDatabasePage(w, userName, databaseName)
 }
 
 // Read the server configuration file
@@ -492,4 +281,299 @@ func readConfig() error {
 
 	// The configuration file seems good
 	return nil
+}
+
+func renderDatabasePage(w http.ResponseWriter, userName string, databaseName string) {
+	pageName := "Render Database Page"
+
+	// Retrieve the MinioID, and the user visible info for the requested database
+	rows, err := db.Query(
+		"SELECT minioid, date_created, last_modified, size, version, public, watchers, stars, forks, "+
+			"discussions, pull_requests, updates, branches, releases, contributors, description, readme "+
+			"FROM public.sqlite_databases "+
+			"WHERE username = $1 "+
+			"AND dbname = $2 "+
+			"ORDER BY version DESC "+
+			"LIMIT 1",
+		userName, databaseName)
+	if err != nil {
+		log.Printf("%s: Database query failed: \n%v", pageName, err)
+		http.Error(w, "Database query failed", http.StatusInternalServerError)
+		return
+	}
+	defer rows.Close()
+	var dataRows dbInfo
+	for rows.Next() {
+		var Desc pgx.NullString
+		var Readme pgx.NullString
+		err = rows.Scan(&dataRows.MinioID, &dataRows.DateCreated, &dataRows.LastModified, &dataRows.Size,
+			&dataRows.Version, &dataRows.Public, &dataRows.Watchers, &dataRows.Stars, &dataRows.Forks,
+			&dataRows.Discussions, &dataRows.PRs, &dataRows.Updates, &dataRows.Branches, &dataRows.Releases,
+			&dataRows.Contributors, &Desc, &Readme)
+		if err != nil {
+			log.Printf("%s: Error retrieving MinioID from database: %v\n", pageName, err)
+			http.Error(w, "Error retrieving MinioID from database", http.StatusInternalServerError)
+			return
+		}
+		if !Desc.Valid {
+			dataRows.Description = "No description"
+		} else {
+			dataRows.Description = Desc.String
+		}
+		if !Readme.Valid {
+			dataRows.Readme = "No readme"
+		} else {
+			dataRows.Readme = Readme.String
+		}
+	}
+	if dataRows.MinioID == "" {
+		log.Printf("%s: Requested database not found: %v for user: %v \n", pageName, databaseName, userName)
+		http.Error(w, "The requested database doesn't exist", http.StatusNotFound)
+		return
+	}
+
+	// Get a handle from Minio for the database object
+	userDB, err := minioClient.GetObject(userName, dataRows.MinioID)
+	if err != nil {
+		log.Printf("%s: Error retrieving DB from Minio: %v\n", pageName, err)
+		http.Error(w, "Error retrieving DB from Minio", http.StatusInternalServerError)
+		return
+	}
+
+	// Close the object handle when this function finishes
+	defer func() {
+		err := userDB.Close()
+		if err != nil {
+			log.Printf("%s: Error closing object handle: %v\n", pageName, err)
+		}
+	}()
+
+	// Save the database locally to a temporary file
+	tempfileHandle, err := ioutil.TempFile("", "databaseViewHandler-")
+	if err != nil {
+		log.Printf("%s: Error creating tempfile: %v\n", pageName, err)
+		http.Error(w, "Internal server error", http.StatusInternalServerError)
+		return
+	}
+	tempfile := tempfileHandle.Name()
+	bytesWritten, err := io.Copy(tempfileHandle, userDB)
+	if err != nil {
+		log.Printf("%s: Error writing database to temporary file: %v\n", pageName, err)
+		http.Error(w, "Internal server error", http.StatusInternalServerError)
+		return
+	}
+	if bytesWritten == 0 {
+		log.Printf("%s: 0 bytes written to the temporary file: %v\n", pageName, databaseName)
+		http.Error(w, "Internal server error", http.StatusInternalServerError)
+		return
+	}
+	tempfileHandle.Close()
+	defer os.Remove(tempfile) // Delete the temporary file when this function finishes
+
+	// Open database
+	db, err := sqlite.Open(tempfile, sqlite.OpenReadOnly)
+	if err != nil {
+		log.Printf("Couldn't open database: %s", err)
+		return
+	}
+	defer db.Close()
+
+	// Retrieve the list of tables in the database
+	tables, err := db.Tables("")
+	if err != nil {
+		log.Printf("Error retrieving table names: %s", err)
+		// TODO: Add proper error handing here.  Maybe display the page, but show the error where
+		// TODO  the table data would otherwise be?
+		http.Error(w, fmt.Sprintf("Error reading from '%s'.  Possibly encrypted or not a database?",
+			databaseName), http.StatusInternalServerError)
+		return
+	}
+	if len(tables) == 0 {
+		// No table names were returned, so abort
+		log.Printf("The database '%s' doesn't seem to have any tables. Aborting.", databaseName)
+		return
+	}
+	dataRows.Tables = tables
+
+	// Select the first table
+	selectedTable := dataRows.Tables[0]
+
+	// Retrieve (up to) x rows from the selected database
+	// Ugh, have to use string smashing for this, even though the SQL spec doesn't seem to say table names
+	// shouldn't be parameterised.  Limitation from SQLite's implementation? :(
+	stmt, err := db.Prepare("SELECT * FROM " + selectedTable + " LIMIT 8")
+	if err != nil {
+		log.Printf("Error when preparing statement for database: %s\v", err)
+		return
+	}
+
+	// Retrieve the field names
+	dataRows.TableHeaders = stmt.ColumnNames()
+
+	// Process each row
+	fieldCount := -1
+	err = stmt.Select(func(s *sqlite.Stmt) error {
+
+		// Get the number of fields in the result
+		if fieldCount == -1 {
+			fieldCount = stmt.DataCount()
+		}
+
+		// Retrieve the data for each row
+		var row []dataValue
+		for i := 0; i < fieldCount; i++ {
+			// Retrieve the data type for the field
+			fieldType := stmt.ColumnType(i)
+
+			switch fieldType {
+			case sqlite.Integer:
+				val, isNull, err := s.ScanInt(i)
+				if err != nil {
+					log.Printf("Something went wrong with ScanInt(): %v\n", err)
+					break
+				}
+				if !isNull {
+					stringVal := fmt.Sprintf("%d", val)
+					row = append(row, dataValue{Name: dataRows.TableHeaders[i], Type: Integer,
+						Value: stringVal})
+				} else {
+					row = append(row, dataValue{Name: dataRows.TableHeaders[i], Type: Null,
+						Value: "NULL"})
+				}
+			case sqlite.Float:
+				val, isNull, err := s.ScanDouble(i)
+				if err != nil {
+					log.Printf("Something went wrong with ScanDouble(): %v\n", err)
+					break
+				}
+				if !isNull {
+					stringVal := strconv.FormatFloat(val, 'f', 4, 64)
+					row = append(row, dataValue{Name: dataRows.TableHeaders[i], Type: Float,
+						Value: stringVal})
+				} else {
+					row = append(row, dataValue{Name: dataRows.TableHeaders[i], Type: Null,
+						Value: "NULL"})
+				}
+			case sqlite.Text:
+				val, isNull := s.ScanText(i)
+				if !isNull {
+					row = append(row, dataValue{Name: dataRows.TableHeaders[i], Type: Text,
+						Value: val})
+				} else {
+					row = append(row, dataValue{Name: dataRows.TableHeaders[i], Type: Null,
+						Value: "NULL"})
+				}
+			case sqlite.Blob:
+				val, isNull := s.ScanBlob(i)
+				if !isNull {
+					row = append(row, dataValue{Name: dataRows.TableHeaders[i], Type: Binary,
+						Value: val})
+				} else {
+					row = append(row, dataValue{Name: dataRows.TableHeaders[i], Type: Null,
+						Value: "NULL"})
+				}
+			case sqlite.Null:
+				row = append(row, dataValue{Name: dataRows.TableHeaders[i], Type: Null,
+					Value: "NULL"})
+			}
+		}
+		dataRows.Records = append(dataRows.Records, row)
+
+		return nil
+	})
+	if err != nil {
+		log.Printf("Error when retrieving select data from database: %s\v", err)
+		http.Error(w, fmt.Sprintf("Error reading data from '%s'.  Possibly malformed?", databaseName),
+			http.StatusInternalServerError)
+		return
+	}
+	defer stmt.Finalize()
+
+	dataRows.Username = userName
+	dataRows.Database = databaseName
+	dataRows.Tablename = selectedTable
+	dataRows.Protocol = listenProtocol
+	dataRows.Server = listenAddr + ":9080"
+
+	// Parse the template, but use "[[" and "]]" as delimiters.  This is because both Go and AngularJS use
+	// "{{" "}}" by default, so one needs to be changed ;)
+	t := template.New("database.html")
+	t.Delims("[[", "]]")
+	t, err = t.ParseFiles("templates/database.html")
+	if err != nil {
+		log.Printf("Error: %s", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	err = t.Execute(w, dataRows)
+	if err != nil {
+		log.Printf("Error: %s", err)
+	}
+}
+
+func renderRootPage(w http.ResponseWriter) {
+	// TODO: Create a template for the root directory
+	fmt.Fprintf(w, "Root directory")
+
+	return
+}
+
+func renderUserPage(w http.ResponseWriter, userName string) {
+	pageName := "User Page"
+
+	// Structure to hold user data
+	var userData struct {
+		Username string
+		DataRows []dbInfo
+	}
+	userData.Username = userName
+
+	// Retrieve list of public databases for the user
+	rows, err := db.Query(
+		"SELECT DISTINCT ON (dbname) dbname, last_modified, size, version, watchers, stars, forks, "+
+			"discussions, pull_requests, updates, branches, releases, contributors, description "+
+			"FROM public.sqlite_databases "+
+			"WHERE username = $1 "+
+			"AND public = TRUE "+
+			"ORDER BY dbname, last_modified DESC",
+		userName)
+	if err != nil {
+		log.Printf("%s: Database query failed: \n%v", pageName, err)
+		http.Error(w, "Database query failed", http.StatusInternalServerError)
+		return
+	}
+	defer rows.Close()
+	for rows.Next() {
+		var oneRow dbInfo
+		var Desc pgx.NullString
+		err = rows.Scan(&oneRow.Database, &oneRow.LastModified, &oneRow.Size, &oneRow.Version,
+			&oneRow.Watchers, &oneRow.Stars, &oneRow.Forks, &oneRow.Discussions, &oneRow.PRs,
+			&oneRow.Updates, &oneRow.Branches, &oneRow.Releases, &oneRow.Contributors, &Desc)
+		if err != nil {
+			log.Printf("%s: Error retrieving database list for user: %v\n", pageName, err)
+			http.Error(w, "Error retrieving database list for user", http.StatusInternalServerError)
+			return
+		}
+		if !Desc.Valid {
+			oneRow.Description = ""
+		} else {
+			oneRow.Description = fmt.Sprintf(": %s", Desc.String)
+		}
+		userData.DataRows = append(userData.DataRows, oneRow)
+	}
+
+	// Parse and execute the template
+	t := template.New("user.html")
+	t.Delims("[[", "]]")
+	t, err = t.ParseFiles("templates/user.html")
+	if err != nil {
+		log.Printf("Error: %s", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	err = t.Execute(w, userData)
+	if err != nil {
+		log.Printf("Error: %s", err)
+	}
 }
