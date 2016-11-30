e64e77e6cd3d1d307d20303be19107780495a29c
Justin Clift <justin@postgresql.org>
2016-11-30 13:26:37 +0000

    Added validation function for email addresses

--- validate.go
+++ validate.go
@@ -1,14 +1,53 @@
 package main
 
-// Validate the provided user and database name
-func validateUserDB(user string, db string) error {
+
+// Validate the provided email address
+func validateEmail(email string) error {
+
+	errs := validate.Var(email, "required,email")
+	if errs != nil {
+		return errs
+	}
+
+	return nil
+}
+
+// Validate the provided username
+func validateUser(user string) error {
 
 	errs := validate.Var(user, "required,alphanum,min=3,max=63")
 	if errs != nil {
 		return errs
 	}
 
+	return nil
+}
+
+// Validate the provided user and database name
+func validateUserDB(user string, db string) error {
+
+	errs := validateUser(user)
+	if errs != nil {
+		return errs
+	}
+
 	errs = validate.Var(db, "required,alphanum|contains=.,min=1,max=1024")
+	if errs != nil {
+		return errs
+	}
+
+	return nil
+}
+
+// Validate the provided username and email address
+func validateUserEmail(user string, email string) error {
+
+	errs := validateUser(user)
+	if errs != nil {
+		return errs
+	}
+
+	errs = validateEmail(email)
 	if errs != nil {
 		return errs
 	}
