2e432aa923452e52cad13264d95a0ab1b9f0f1b7
Martin Kleusberg <mkleusberg@gmail.com>
2017-03-17 20:00:23 +0100

    Fix whitespace

--- db4s/main.go
+++ db4s/main.go
@@ -411,8 +411,8 @@ func userDatabaseList(pageName string, user string) (dbList []byte, err error) {
 
 	// Structure to hold the results, to apply JSON marshalling to
 	type linkRow struct {
-		Type       string `json:"type"`
-		Name       string `json:"name"`
+		Type         string `json:"type"`
+		Name         string `json:"name"`
 		Version      int    `json:"version"`
 		URL          string `json:"url"`
 		Size         int    `json:"size"`
