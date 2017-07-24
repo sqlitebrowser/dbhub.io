18b75468db6b9a0c56af1ca85c387e32655d0d48
Justin Clift <justin@postgresql.org>
2017-07-24 20:01:16 +0100

    Input validation for updateTagHandler()

--- webui/main.go
+++ webui/main.go
@@ -2683,36 +2683,45 @@ func updateTagHandler(w http.ResponseWriter, r *http.Request) {
 
 	// Ensure we have a valid logged in user
 	if validSession != true {
-		errorPage(w, r, http.StatusUnauthorized, "You need to be logged in")
+		w.WriteHeader(http.StatusUnauthorized)
 		return
 	}
 
 	// Extract the required form variables
-	dbFolder := r.PostFormValue("dbFolder")
-	dbName := r.PostFormValue("dbName")
-	dbOwner := r.PostFormValue("dbOwner")
-	newMsg := r.PostFormValue("newDesc")
-	newName := r.PostFormValue("newName")
+	u, dbFolder, dbName, err := com.GetFormUFD(r)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	dbOwner := strings.ToLower(u)
+
+	// Validate new tag name
+	nt := r.PostFormValue("newtag")
+	err = com.Validate.Var(nt, "branchortagname,min=1,max=32") // 32 seems a reasonable first guess.
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	newName := nt
+
+	// Validate new tag message
+	nm := r.PostFormValue("newmsg")
+	err = com.Validate.Var(nm, "markdownsource")
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	newMsg := nm
 
 	// Ensure a tag name was supplied
 	tagName, err := com.GetFormTag(r)
 	if err != nil {
-		errorPage(w, r, http.StatusBadRequest, "Missing or incorrect tag name")
+		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
 
 	// If any of the required values were empty, indicate failure
 	if tagName == "" || dbFolder == "" || dbName == "" || dbOwner == "" || newMsg == "" || newName == "" {
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
-
-	// TODO: Validate the variables
-
-	// Validate the database name
-	err = com.ValidateDB(dbName)
-	if err != nil {
-		log.Printf("%s: Validation failed for database name: %s", pageName, err)
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
