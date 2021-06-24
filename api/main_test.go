d71bb03be09cf5efb7c3cb7f93908381ff697436
Justin Clift <justin@postgresql.org>
2021-06-25 02:03:04 +1000

    WIP. Stub test go file, for fleshing out once we have the Docker bit ready
    Not feeling well atm, so prob not doing anything more than this tonight.

---
+++ api/main_test.go
@@ -0,0 +1,12 @@
+package main
+
+// Initial tests for the API functions
+//   * These are designed to run in a Docker container, with the DBHub.io
+//     daemons running in the same container (listening on localhost), with
+//     the testing database loaded
+
+import "testing"
+
+func TestSomething(t *testing.T) {
+	// TBD
+}
