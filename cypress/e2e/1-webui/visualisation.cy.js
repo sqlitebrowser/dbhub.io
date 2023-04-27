sqlitebrowser/dbhub.io

commit 7b2caad079fcf09fcd3032c0ffd9362cb13bf2a9

webui: Ensure Save/Delete buttons are only shown to database owner
We probably want to allow anyone with write access to see and use
them, but this is a reasonable starting point

webui/templates/visualise.html
@@ -166,9 +166,11 @@
         <input type="submit" class="btn" value="Download as CSV" ng-click="downloadResults()" data-cy="downcsvbtn">
         <input type="submit" class="btn" value="Format SQL" ng-click="formatSQL()" data-cy="formatsqlbtn">
         <input type="submit" class="btn btn-success" value="Run SQL" ng-click="execSQL()" data-cy="runsqlbtn">
-        <input type="submit" class="btn btn-primary" value="Save as:" ng-click="saveAs()" data-cy="savebtn">
-        <input type="text" id="savename" value="default" data-cy="nameinput">
-        <input type="submit" class="btn btn-danger" value="Delete" ng-click="deleteVis()" data-cy="delvisbtn">
+        [[ if eq .PageMeta.LoggedInUser .DB.Info.Owner ]]
+            <input type="submit" class="btn btn-primary" value="Save as:" ng-click="saveAs()" data-cy="savebtn">
+            <input type="text" id="savename" value="default" data-cy="nameinput">
+            <input type="submit" class="btn btn-danger" value="Delete" ng-click="deleteVis()" data-cy="delvisbtn">
+        [[ end ]]
     </div>
     <div class="row">
         <div class="col-md-2">&nbsp;</div>

cypress/e2e/1-webui/visualisation.cy.js
@@ -322,4 +322,40 @@ describe('visualisation', () => {
     // Switch back to the default user
     cy.request('/x/test/switchdefault')
   })
+
+  // Ensure the save and delete buttons are only shown to the database owner
+  it('Ensure save/delete buttons are only shown to database owner', () => {
+    // Switch to a different user
+    cy.request('/x/test/switchfirst')
+
+    // Load a public page
+    cy.visit('/vis/default/Assembly Election 2017 with view.sqlite')
+
+    // Test if the Save and Delete fields are visible.  They shouldn't be
+    cy.get('[data-cy="savebtn"').should('not.exist')
+    cy.get('[data-cy="nameinput"').should('not.exist')
+    cy.get('[data-cy="delvisbtn"').should('not.exist')
+
+    // Log out
+    cy.request('/x/test/logout')
+
+    // Reload the page
+    cy.visit('/vis/default/Assembly Election 2017 with view.sqlite')
+
+    // Test if the Save and Delete fields are visible.  They shouldn't be
+    cy.get('[data-cy="savebtn"').should('not.exist')
+    cy.get('[data-cy="nameinput"').should('not.exist')
+    cy.get('[data-cy="delvisbtn"').should('not.exist')
+
+    // Switch back to the default user
+    cy.request('/x/test/switchdefault')
+
+    // Reload the page again
+    cy.visit('/vis/default/Assembly Election 2017 with view.sqlite')
+
+    // Ensure the Save and Delete fields are visible.  They should be this time
+    cy.get('[data-cy="savebtn"').should('exist')
+    cy.get('[data-cy="nameinput"').should('exist')
+    cy.get('[data-cy="delvisbtn"').should('exist')
+  })
 })

commit 6b2e460f81343d082b9ccc8f558e33a146ef5b78

webui, cypress: Fix bug stopping logged-out users from using public visualisations
Introduced 2 days ago in 7b2caad. :(

This commit also adds a Cypress test, to ensure we don't
accidentally re-introduce this bug at some point.

webui/templates/visualise.html
@@ -441,6 +441,7 @@
         // Change to a newly selected visualisation
         // Note, the 'execNewVis' parameter controls whether to execute and display the visualisation after loading it
         $scope.changeVis = function(newVis, execNewVis) {
+
             // Clear out any existing table data and chart
             $scope.showVis = false;
             $scope.clearData();
@@ -454,7 +455,10 @@
                     }
 
                     // Apply the retrieved information
-                    document.getElementById("savename").value = newVis;
+                    let saveTxt = document.getElementById("savename");
+                    if (saveTxt) {
+                        saveTxt.value = newVis;
+                    }
                     $scope.visName = newVis;
 
                     // We include '.textContent' here now because Cypress (v12.3.0) *refuses* to return the correct

cypress/e2e/1-webui/visualisation.cy.js
@@ -358,4 +358,51 @@ describe('visualisation', () => {
     cy.get('[data-cy="nameinput"').should('exist')
     cy.get('[data-cy="delvisbtn"').should('exist')
   })
+
+  // Ensure a logged-out user can still change visualisations
+  it('Ensure a logged-out user can still change visualisations', () => {
+    // Switch to the default user
+    cy.request('/x/test/switchdefault')
+
+    // Load a public database
+    cy.visit('/vis/default/Assembly Election 2017 with view.sqlite')
+
+    // Delete the "default" visualisation
+    cy.get('[data-cy="visdropdown"]').click()
+    cy.get('[data-cy="vis-default"]').click()
+    cy.get('[data-cy="delvisbtn"]').click()
+
+    // Create a second visualisation
+    cy.get('[data-cy="sqltab"]').click()
+    cy.get('[data-cy="usersqltext"]').type('{selectall}{backspace}').type(
+        'SELECT Constituency_Name, Constituency_Number\n' +
+        'FROM Constituency_Turnout_Information\n' +
+        'ORDER BY Constituency_Name ASC\n' +
+        'LIMIT 5')
+    cy.get('[data-cy="nameinput"]').type('{selectall}{backspace}').type('test2 ASC')
+    cy.get('[data-cy="savebtn"]').click()
+
+    // Log out
+    cy.request('/x/test/logout')
+
+    // Load the database again
+    cy.visit('/vis/default/Assembly Election 2017 with view.sqlite')
+
+    // Ensure the visualisation drop down works ok
+    cy.get('[data-cy="visdropdown"]').click()
+    cy.get('[data-cy="vis-test2 ASC"]').click()
+    cy.get('[data-cy="usersqltext"]').should('contain.text', 'SELECT Constituency_Name, Constituency_Number\n' +
+        'FROM Constituency_Turnout_Information\n' +
+        'ORDER BY Constituency_Name ASC\n' +
+        'LIMIT 5')
+    cy.get('[data-cy="visdropdown"]').click()
+    cy.get('[data-cy="vis-test1"]').click()
+    cy.get('[data-cy="usersqltext"]').should('contain.text', 'SELECT Constituency_Name, Constituency_Number, Turnout_pct\n' +
+        'FROM Constituency_Turnout_Information\n' +
+        'ORDER BY Constituency_Name DESC\n' +
+        'LIMIT 10')
+
+    // Switch back to the default user
+    cy.request('/x/test/switchdefault')
+  })
 })