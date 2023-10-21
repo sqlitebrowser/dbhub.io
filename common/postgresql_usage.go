65910ef627635d2b962b7384f39c7e62ecb20d6e
Justin Clift <justin@postgresql.org>
2023-10-21 13:46:03 +1000

    Disk usage function now returns the highest value for each month

--- common/postgresql_usage.go
+++ common/postgresql_usage.go
@@ -13,17 +13,19 @@ type NumDatabases struct {
 
 // UsageDiskSpaceUser returns the historical and current amount of disk space used by a given user
 func UsageDiskSpaceUser(username string) (usage []NumDatabases, err error) {
+	// FIXME: Manually verify this is indeed returning the highest value recorded in any month, just to be super safe
 	dbQuery := `
 		WITH loggedIn AS (
 			SELECT user_id
 			FROM users
 			WHERE lower(user_name) = lower($1)
 		)
-		SELECT to_char(analysis_date, 'YYYY-MM') AS "Usage date", max(standard_databases_bytes) / (1024*1024) AS "Standard databases" OVER (PARTITION BY "Usage date"), max(live_databases_bytes) / (1024*1024) AS "Live databases" OVER (PARTITION BY "Usage date")
+		SELECT to_char(analysis_date, 'YYYY-MM') AS "Usage date", max(standard_databases_bytes) OVER w / (1024*1024) AS "Standard databases", max(live_databases_bytes) OVER w / (1024*1024) AS "Live databases"
 		FROM analysis_space_used a, users u
 		WHERE a.user_id = u.user_id
 			AND u.user_id = (SELECT user_id FROM loggedIn)
 			AND standard_databases_bytes > 0
+		WINDOW w AS (PARTITION BY to_char(analysis_date, 'YYYY-MM'))
 		ORDER BY "Usage date" ASC`
 	rows, err := pdb.Query(context.Background(), dbQuery, username)
 	if err != nil {
