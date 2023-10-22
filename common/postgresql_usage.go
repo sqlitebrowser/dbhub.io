d1807eb6855212b3f3315d8c4c765bbc8b43e681
Justin Clift <justin@postgresql.org>
2023-10-22 21:39:07 +1000

    WIP.  Initial working code to display historical upload numbers

--- common/postgresql_usage.go
+++ common/postgresql_usage.go
@@ -11,8 +11,15 @@ type NumDatabases struct {
 	NumLive int `json:"num_live"`
 }
 
-// UsageDiskSpaceUserHistorical returns the historical and current amount of disk space used by a given user
-func UsageDiskSpaceUserHistorical(username string) (usage []NumDatabases, err error) {
+type NumUploads struct {
+	UploadDate string `json:"upload_date"`
+	NumApi int `json:"num_api"`
+	NumDB4S int `json:"num_db4s"`
+	NumWebui  int `json:"num_webui"`
+}
+
+// UsageUserDiskSpaceHistorical returns the historical amount of disk space used by a given user
+func UsageUserDiskSpaceHistorical(username string) (usage []NumDatabases, err error) {
 	// FIXME: Manually verify this is indeed returning the highest value recorded in any month, just to be super safe
 	dbQuery := `
 		WITH loggedIn AS (
@@ -50,8 +57,8 @@ func UsageDiskSpaceUserHistorical(username string) (usage []NumDatabases, err er
 	return
 }
 
-// UsageDiskSpaceUserRecent returns the amount of disk space used by a given user over the last 30 days
-func UsageDiskSpaceUserRecent(username string) (usage []NumDatabases, err error) {
+// UsageUserDiskSpaceRecent returns the amount of disk space used by a given user over the last 30 days
+func UsageUserDiskSpaceRecent(username string) (usage []NumDatabases, err error) {
 	dbQuery := `
 		WITH loggedIn AS (
 			SELECT user_id
@@ -85,5 +92,62 @@ func UsageDiskSpaceUserRecent(username string) (usage []NumDatabases, err error)
 			NumLive: numLive,
 		})
 	}
+	return
+}
+
+// UsageUserUploadsHistorical returns the historical number of uploads by a given user
+func UsageUserUploadsHistorical(username string) (usage []NumUploads, err error) {
+	dbQuery := `
+		WITH loggedIn AS (
+			SELECT user_id
+			FROM users
+			WHERE lower(user_name) = lower($1)
+		)
+		SELECT to_char(d.upload_date, 'YYYY-MM') AS "Upload date", d.server_sw, count(u.email)
+		FROM database_uploads d, users u
+		WHERE d.user_id = u.user_id
+			AND u.user_id = (SELECT user_id FROM loggedIn)
+		GROUP BY "Upload date", d.server_sw
+		ORDER BY "Upload date"`
+	rows, err := pdb.Query(context.Background(), dbQuery, username)
+	if err != nil {
+		log.Printf("Database query failed in %s: %v", GetCurrentFunctionName(), err)
+		return
+	}
+	defer rows.Close()
+	var tmpUploadCount NumUploads
+	for rows.Next() {
+		var date, serverSw string
+		var uploadCount int
+		err = rows.Scan(&date, &serverSw, &uploadCount)
+		if err != nil {
+			log.Printf("Error in %s when retrieving the historical upload data for for '%s': %v", GetCurrentFunctionName(), username, err)
+			return nil, err
+		}
+		if tmpUploadCount.UploadDate != date {
+			// We've moved on to a new date, so store the existing one we're processing in the output slice
+			if tmpUploadCount.UploadDate != "" {
+				usage = append(usage, tmpUploadCount)
+			}
+
+			// Clear the existing entries in the temporary upload data buffer
+			tmpUploadCount.UploadDate = ""
+			tmpUploadCount.NumApi = 0
+			tmpUploadCount.NumDB4S = 0
+			tmpUploadCount.NumWebui = 0
+		}
+		tmpUploadCount.UploadDate = date
+		if serverSw == "api" && uploadCount != 0 {
+			tmpUploadCount.NumApi = uploadCount
+		}
+		if serverSw == "db4s" && uploadCount != 0 {
+			tmpUploadCount.NumDB4S = uploadCount
+		}
+		if serverSw == "webui" && uploadCount != 0 {
+			tmpUploadCount.NumWebui = uploadCount
+		}
+	}
+	// Add the final temporary entry to the output slice
+	usage = append(usage, tmpUploadCount)
 	return
 }
