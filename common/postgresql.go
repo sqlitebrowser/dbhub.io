0f7a52a965901e5c19738dfd7494f7b44ad48538
Justin Clift <justin@postgresql.org>
2023-10-25 20:46:52 +1000

    WIP.  Update analysis util to record the number of databases for each user

--- common/postgresql.go
+++ common/postgresql.go
@@ -104,22 +104,22 @@ func AddUser(auth0ID, userName, password, email, displayName, avatarURL string) 
 }
 
 // AnalysisRecordUserStorage adds a record to the backend database containing the amount of storage space used by a user
-func AnalysisRecordUserStorage(userName string, recordDate time.Time, spaceUsedStandard, spaceUsedLive int64) (err error) {
+func AnalysisRecordUserStorage(userName string, recordDate time.Time, spaceUsedStandard, spaceUsedLive int64, numStandardDB, numLiveDB int) (err error) {
 	dbQuery := `
 		WITH u AS (
 			SELECT user_id
 			FROM users
 			WHERE lower(user_name) = lower($1)
 		)
-		INSERT INTO analysis_space_used (user_id, analysis_date, standard_databases_bytes, live_databases_bytes)
-		VALUES ((SELECT user_id FROM u), $2, $3, $4)`
-	commandTag, err := pdb.Exec(context.Background(), dbQuery, userName, recordDate, spaceUsedStandard, spaceUsedLive)
+		INSERT INTO analysis_space_used (user_id, analysis_date, standard_databases_bytes, live_databases_bytes, standard_databases_qty, live_databases_qty)
+		VALUES ((SELECT user_id FROM u), $2, $3, $4, $5, $6)`
+	commandTag, err := pdb.Exec(context.Background(), dbQuery, userName, recordDate, spaceUsedStandard, spaceUsedLive, numStandardDB, numLiveDB)
 	if err != nil {
-		log.Printf("Adding record of storage space used by '%s' failed: %s", userName, err)
+		log.Printf("Adding record of storage space used and database qty by '%s' failed: %s", userName, err)
 		return
 	}
 	if numRows := commandTag.RowsAffected(); numRows != 1 {
-		log.Printf("Wrong number of rows (%d) affected when recording the storage space used by '%s'", numRows, userName)
+		log.Printf("Wrong number of rows (%d) affected when recording the storage space used and database qty by '%s'", numRows, userName)
 	}
 	return
 }
